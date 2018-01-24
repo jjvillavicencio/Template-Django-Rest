b269921adfa7eb19297673ca2dd429c8499be68a
jjvillavicencio <jjvillavicencio1@gmail.com>
2018-01-24 01:31:52 -0500

    Añadir modelo al administrador

--- mercalistAPI/admin.py
+++ mercalistAPI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from mercalistAPI.models import *
 
-# Register your models here.
+admin.site.register(TipoPago)
+admin.site.register(Categoria)
+admin.site.register(Entrega)
+admin.site.register(Supermercado)
+admin.site.register(Usuario)
+admin.site.register(Sucursal)
+admin.site.register(Producto)
+admin.site.register(Lista)
+admin.site.register(Compra)
